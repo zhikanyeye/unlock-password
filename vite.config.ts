4edf4343543448f89ffa4a0235e913ce8f7340ba
zhikanyeye <ai2845612055@outlook.com>
2025-03-02 20:25:50 +0800

    更新JiaPass项目代码

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
